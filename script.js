601c70a322700ae1d0cf3582c933bef20bf545d3
Ragip Gjinovci <ragipgjinovci@gmail.com>
2020-03-15 14:55:39 +0100

    Added sliders functionality, blog slider needs work

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Scroll to top
+
 mybutton = document.querySelector(".up-btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -18,3 +20,61 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Testimonials slider
+$(".slider").slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 5,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+
+// Blog slider
+$('.blog-items').slick({
+    centerMode: true,
+    centerPadding: '200px',
+    slidesToShow: 4,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
+$(".slick-slide[data-slick-index='-3']").css("opacity", ".5");
+$(".blog-items .slick-slide[data-slick-index='2']").css("opacity", ".5");
