ba8efa386fabdb3b4f4db17b572ff21e6ab4d815
Fatbardh <fatbardhemini5@gmail.com>
2020-03-27 20:21:32 -0700

    Desktop version

--- app.js
+++ app.js
@@ -9,33 +9,65 @@ $(document).ready(function(){
 });
 
 
-// $(document).ready(function(){
-// 	$(".clients_container").slick({
-// 		centerMode: true,
-// 		infinite:true,
-// 		centerPadding: '60px',
-// 		slidesToShow: 3,
-// 		prevArrow: $(".client_left"),
-// 		nextArrow: $(".client_right"),
-// 		responsive: [
-// 		  {
-// 			breakpoint: 768,
-// 			settings: {
-// 			  arrows: false,
-// 			  centerMode: true,
-// 			  centerPadding: '40px',
-// 			  slidesToShow: 3
-// 			}
-// 		  },
-// 		  {
-// 			breakpoint: 480,
-// 			settings: {
-// 			  arrows: false,
-// 			  centerMode: true,
-// 			  centerPadding: '40px',
-// 			  slidesToShow: 1
-// 			}
-// 		  }
-// 		]
-// 	  });
-// });
+$(document).ready(function(){
+	$(".clients_container").slick({
+		centerMode: true,
+		centerPadding: '0px',
+		slidesToShow: 5,
+		prevArrow: $(".client_left"),
+		nextArrow: $(".client_right"),
+		responsive: [
+		  {
+			breakpoint: 768,
+			settings: {
+			  centerMode: true,
+			  centerPadding: '0px',
+			  slidesToShow: 3
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  centerMode: true,
+			  centerPadding: '0px',
+			  slidesToShow: 1
+			}
+		  }
+		]
+	  });
+});
+
+//third slider(blog slider)
+
+$(document).ready(function(){
+	$(".blog_container").slick({
+		infinite: true,
+		arrows:false,
+		dots : false,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		slidesToShow:4,
+		centerMode:true,
+		centerPadding: '200px'
+		// responsive: [
+		// 	{
+		// 	  breakpoint: 768,
+		// 	  settings: {
+		// 		arrows: false,
+		// 		centerMode: true,
+		// 		centerPadding: '40px',
+		// 		slidesToShow: 3
+		// 	  }
+		// 	},
+		// 	{
+		// 	  breakpoint: 480,
+		// 	  settings: {
+		// 		arrows: false,
+		// 		centerMode: true,
+		// 		centerPadding: '40px',
+		// 		slidesToShow: 1
+		// 	  }
+		// 	}
+		//   ]
+		});
+	});
