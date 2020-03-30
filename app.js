323f87cb4ee3f623840a2843a5a035a2d86af4f7
Fatbardh <fatbardhemini5@gmail.com>
2020-03-29 18:33:57 -0700

    Version 2.0 with responsive

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//header_slider
 $(document).ready(function(){
 	$(".slider1_part").slick({
 		infinite: true,
@@ -9,6 +10,8 @@ $(document).ready(function(){
 });
 
 
+//client slider
+
 $(document).ready(function(){
 	$(".clients_container").slick({
 		centerMode: true,
@@ -17,6 +20,12 @@ $(document).ready(function(){
 		prevArrow: $(".client_left"),
 		nextArrow: $(".client_right"),
 		responsive: [
+			{
+				breakpoint :1124,
+				settings:{
+					slidesToShow:3
+				}
+			},
 		  {
 			breakpoint: 768,
 			settings: {
@@ -26,18 +35,21 @@ $(document).ready(function(){
 			}
 		  },
 		  {
-			breakpoint: 480,
+			breakpoint: 621,
 			settings: {
 			  centerMode: true,
 			  centerPadding: '0px',
-			  slidesToShow: 1
+			  slidesToShow: 1,
+			  slidesToScroll:1,
+			  dots:true,
+			  arrows:false
 			}
 		  }
 		]
 	  });
 });
 
-//third slider(blog slider)
+//(blog slider)
 
 $(document).ready(function(){
 	$(".blog_container").slick({
@@ -48,26 +60,87 @@ $(document).ready(function(){
 		autoplaySpeed: 3000,
 		slidesToShow:4,
 		centerMode:true,
-		centerPadding: '200px'
-		// responsive: [
-		// 	{
-		// 	  breakpoint: 768,
-		// 	  settings: {
-		// 		arrows: false,
-		// 		centerMode: true,
-		// 		centerPadding: '40px',
-		// 		slidesToShow: 3
-		// 	  }
-		// 	},
-		// 	{
-		// 	  breakpoint: 480,
-		// 	  settings: {
-		// 		arrows: false,
-		// 		centerMode: true,
-		// 		centerPadding: '40px',
-		// 		slidesToShow: 1
-		// 	  }
-		// 	}
-		//   ]
+		centerPadding: '200px',
+		responsive: [
+			{
+			  breakpoint: 1124,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '100px',
+				slidesToShow: 3
+			  }
+			},
+			{
+			  breakpoint:620,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			}
+		  ]
 		});
 	});
+
+//courses slider
+
+$(window).on("resize", function() {
+    if( $(this).width() < 620 ) {
+        $(document).ready(function(){
+			$(".courses_container").slick({
+				infinite: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows:false,
+				dots : true
+			});
+		});
+	}
+	else if($(window).width() > 620){
+		return false;
+	}
+});
+
+
+//teachers slider
+
+$(window).on("resize", function() {
+    if( $(this).width() < 620 ) {
+        $(document).ready(function(){
+			$(".teachers_container").slick({
+				centerMode: true,
+				infinite: true,
+				autoplay: true,
+				autoplaySpeed: 3000,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows:false,
+				dots : true
+			});
+		});
+	}
+}).trigger("resize");
+
+
+const hamburger = document.querySelector('.first_header i')
+const pop = document.querySelector('.list_pop')
+const x = document.querySelector('.list_pop i');
+const a = document.querySelector('.list_pop ul');
+const body = document.querySelector("body");
+
+hamburger.addEventListener("click", function(){
+	pop.style.height = '100vh';
+	pop.style.transition = ' height 800ms ease-out';
+	a.style.display='block';
+	x.style.display='block';
+	body.style.overflow='hidden';
+});
+
+x.addEventListener("click", function(){
+	pop.style.height = '0vh';
+	a.style.display='none';
+	x.style.display='none';
+	body.style.overflow='unset';
+});
